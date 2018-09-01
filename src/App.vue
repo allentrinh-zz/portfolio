828fcf0a112b807a41426d5aeff85893eb259f1b
Allen Trinh <allenmtrinh@gmail.com>
2018-09-01 19:21:07 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,148 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="navigation-drawer" :class="{ open: drawerActive }">
+      <router-link to="/">
+        <img src="./assets/allen-trinh.png" class="profile-picture" alt="This is Allen Trinh">
+      </router-link>
+      <nav>
+        <ul class="nav">
+          <li><router-link to="/"><i class="icon ion-ios-home"></i> Home</router-link></li>
+          <li><router-link to="/about"><i class="icon ion-ios-person"></i> About Me</router-link></li>
+          <li><router-link to="/work"><i class="icon ion-ios-paper"></i> Resume</router-link></li>
+          <li><router-link to="/work"><i class="icon ion-ios-code"></i> Work</router-link></li>
+          <li><router-link to="/contact"><i class="icon ion-ios-mail"></i> Contact</router-link></li>
+        </ul>
+      </nav>
     </div>
-    <router-view/>
+    <div id="panel" :class="{ padded: drawerActive }">
+      <header id="header">
+        <div class="mobile-header text-center d-lg-none">
+          <div class="container-fluid">
+            <router-link to="/" class="mobile-logo">Allen <span>Trinh</span></router-link>
+            <button @click="toggleDrawer()" class="toggle-drawer">
+              <i class="icon ion-ios-menu"></i>
+            </button>
+          </div>
+        </div>
+      </header>
+      <main id="content">
+        <router-view/>
+      </main>
+      <Footer/>
+    </div>
   </div>
 </template>
 
+<script>
+import Footer from '@/components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Footer,
+  },
+  data() {
+    return {
+      drawerActive: false,
+    };
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawerActive = !this.drawerActive;
+    },
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import '@/assets/variables.scss';
+@import '@/assets/mixins.scss';
+
+.mobile-header {
+  background: $color;
+  padding: 1em 0;
 }
-#nav {
-  padding: 30px;
+
+.mobile-logo {
+  font-family: $heading;
+  text-transform: uppercase;
+  color: #fff;
+  font-size: 2em;
+  margin: 0;
+  line-height: 1;
+  span {
+    color: $primaryColor;
+  }
+  &:hover {
+    text-decoration: none;
+    color: #fff;
+  }
+}
+
+.toggle-drawer {
+  float: right;
+  background: transparent;
+  border: 0;
+  color: #fff;
+  font-size: 2em;
+  line-height: 1;
+  &:active, &:focus {
+    outline: 0;
+  }
+}
+
+.profile-picture {
+  border-bottom: 10px solid $primaryColor;
+  max-width: 100%;
+}
+
+.navigation-drawer {
+  width: 200px;
+  height: 100%;
+  background: $color;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-right: 1px solid $color;
+  transform: translateX(-100%);
+  @include transition;
+  &.open {
+    transform: translateX(0);
+  }
+  @media (min-width: $screen-lg) {
+    transform: translateX(0);
+  }
+}
+
+.nav {
+  margin: 0;
+  padding: 0;
+  flex-direction: column;
   a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    color: #fff;
+    padding: 1em;
+    font-size: 1.15em;;
+    line-height: 1;
+    display: block;
+    font-family: $heading;
+    letter-spacing: 1px;
+    border-top: 1px solid $primaryColor;
+    @include transition;
+    &:hover {
+      text-decoration: none;
+      background: $primaryColor;
+    }
+  }
+}
+
+#panel {
+  @include transition;
+  @media (min-width: $screen-lg) {
+    padding-left: 200px;
+  }
+  &.padded {
+    @media (min-width: $screen-lg) {
+      padding-left: 200px;
     }
   }
 }
---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer id="footer">
+    <div class="container">
+      This is the footer
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
