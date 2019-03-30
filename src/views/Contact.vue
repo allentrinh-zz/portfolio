29bf0b9d2698d7bbcca96f85b3f9dfe5e285b230
allentrinh <allenmtrinh@gmail.com>
2019-03-30 15:48:49 -0400

    Remove contact form and replace with simple mailto button

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,15 +4,13 @@
     <div class="section content">
       <div class="container">
         <div class="columns">
-          <div class="column">
+          <div class="column is-6 is-offset-3">
             <h2 class="subtitle is-spaced is-5">Let's work together</h2>
             <p class="large-print">If you want to make something fancy but don't have the <em>know-how</em>, let's chat!</p>
+            <a href="mailto:allenmtrinh@gmail.com" class="button ghost">allenmtrinh@gmail.com</a>
             <hr>
             <h4 class="subtitle is-5">Follow me!</h4>
             <SocialLinks/>
-          </div>
-          <div class="column">
-            <ContactForm/>
           </div>
         </div>
       </div>
@@ -31,11 +29,6 @@ export default {
     Hero,
     ContactForm,
     SocialLinks,
-  },
-  data() {
-    return {
-
-    };
   },
 };
 </script>
